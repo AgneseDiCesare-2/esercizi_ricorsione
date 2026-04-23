80e128c348cfaa71cb40231a2b5f4fdfc38cfa91
agnese <s325169@studenti.polito.it>
2026-04-23 10:51:29 +0200

    non ho capito uff

--- regine.py
+++ regine.py
@@ -1,19 +1,25 @@
 #abbiamo una scacchiera 4x4 --> come metto le 4 regine?
+import copy
+
+
 class Scacchiera:
     def __init__(self, ):
         self.num_soluzioni=0
         self.num_chimate=0
+        self._soluzioni=[]
 
     def solve(self, n):
+        self._soluzioni=[] #NB!
         self._ricorsione([], n)
 
 
     def _ricorsione(self, parziale, n):
         self.num_chimate+=1
         if len(parziale)==n:
+            if self.is_nuova_soluzione(parziale):
             #if self._is_soluzione(parziale): diventa superfluo se uso step_valid
                 self.num_soluzioni += 1
-                print(parziale)
+                self._soluzioni.append(copy.deepcopy(parziale)) #su parziale sto facendo pop, quindi considero la copia!
 
         else:
            for riga in range(n):
@@ -45,6 +51,19 @@ class Scacchiera:
         else:
             return True
 
+    #vedo se la configurazione è già presente
+    def is_nuova_soluzione(self, soluzione_potenziale):
+        n=len(soluzione_potenziale)
+        for soluzione in self._soluzioni:
+            counter=0
+            for regina in soluzione_potenziale:
+                if regina in soluzione:
+                    counter+=1
+
+            if counter==n:
+                return False #soluzione già presente
+        return True
+
     #def _is_soluzione(self, soluzione_possible) -> bool:
     #   for i in range(len(soluzione_possible)-1):
     #        for j in range(i+1, len(soluzione_possible)):
@@ -55,5 +74,6 @@ class Scacchiera:
 if __name__=="__main__":
     nreg=Scacchiera()
     nreg.solve(4)
-    print(nreg.num_chimate)
-    print(nreg.num_soluzioni)
+    print(f"numero chiamate: {nreg.num_chimate}")
+    print(f"numero soluzioni: {nreg.num_soluzioni}")
+    print(nreg._soluzioni)
